05d5d9ace824492334fa5c610e72b397e5f78e82
Золотарев Кирилл Сергеевич <kszolot1@mts.ru>
2023-01-13 22:08:16 +0300

    VERSION 1 START

---
+++ src/main/java/io/zolotarev/superbot/service/TelegramBot.java
@@ -0,0 +1,69 @@
+package io.zolotarev.superbot.service;
+import org.springframework.stereotype.Component;
+import io.zolotarev.superbot.config.BotConfig;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config){
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername(){
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken(){
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update){
+        if(update.hasMessage() && update.getMessage().hasText())
+        {
+           String messageText = update.getMessage().getText();
+           long chatId = update.getMessage().getChatId();
+
+           switch (messageText){
+               case "/start":
+                   try {
+                       startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                   } catch (TelegramApiException e) {
+                       throw new RuntimeException(e);
+                   }
+               default:
+                   try {
+                       sendMessage(chatId, "Привет пиписька");
+                   } catch (TelegramApiException e) {
+                       throw new RuntimeException(e);
+                   }
+
+           }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) throws TelegramApiException {
+        String answer = "Привет, "+name+" !";
+        sendMessage(chatId, answer);
+
+    }
+
+    private void sendMessage(long chatId, String textToSend) throws TelegramApiException {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try{
+            execute(message);
+        } catch (TelegramApiException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/io/zolotarev/superbot/SuperbotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zolotarev.superbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
